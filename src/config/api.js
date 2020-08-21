53bc73a28e4ed370656a238fe67952983637b125
Gamal Al Fajri <73198993@MAC0GKG8WKJENIUS.local>
2020-08-21 18:59:47 +0700

    initial Commit Bosss

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://wc2.rnlab.io';
+
+export const CONSUMER_KEY = 'ck_66ba9f876d6dd8db9a15daac39eecbd53579e0c8';
+export const CONSUMER_SECRET = 'cs_b45e5895f2af5e4ec9b51a46dc556fd572c85c49';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
