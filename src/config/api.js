8d62c3b490fe315fb169e3a9efc0c5a65f30debf
herdiansyah <herdiiyan8@gmail.com>
2020-09-09 13:31:40 +0700

    [IMPROVE] ganti API config

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const API = 'https://wc2.rnlab.io';
+export const API = 'https://kopebitest.com';
 
 export const CONSUMER_KEY = 'ck_66ba9f876d6dd8db9a15daac39eecbd53579e0c8';
 export const CONSUMER_SECRET = 'cs_b45e5895f2af5e4ec9b51a46dc556fd572c85c49';
